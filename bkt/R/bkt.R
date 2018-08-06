e58114d8b04dbd70272eec37322c8f4694b1f67b
HMT <tuan.hmt@outlook.com>
2018-08-06 19:15:03 +0700

    bkt module modification

--- bkt/R/bkt.R
+++ bkt/R/bkt.R
@@ -2,7 +2,7 @@
 ##########################################################################################################
 # Copyright (C) 2018
 # Based on REDM project (http://www.educationaldatamining.org/). Power by R language.
-# 
+#
 # Tuan Hoang - MSC Student
 # Email: tuanhcmup@gmail.com, tuan.hmt@outlook.com
 # University of Science, HCM City, Vietnam
@@ -14,7 +14,7 @@
 ##
 
 #' coerces various data types to a standard format for BKT parameters
-#' 
+#'
 #' @param p the object to be coerced
 #' @param round.params whether we want to round off the parameter values to 2 decimal-places.
 #' @return a data.frame with four named columns:
@@ -28,29 +28,29 @@
 as.bkt.params <- function(p, round.params = T)
 {
   if(is.list(p))
-    kt.params = as.data.frame(p)
+    kt.params = as.data.table(p)
   else if("init" %in% names(p)){
-    kt.params = data.frame(init=p["init"], learn=p["learn"], guess=p["guess"], slip=p["slip"])
+    kt.params = data.table(init=p["init"], learn=p["learn"], guess=p["guess"], slip=p["slip"])
     rownames(kt.params) = NULL
   } else
-    kt.params = data.frame(init=p[1], learn=p[2], guess=p[3], slip=p[4])
+    kt.params = data.table(init=p[1], learn=p[2], guess=p[3], slip=p[4])
   if(round.params)
     kt.params = round(kt.params, 3)
   kt.params
 }
 
 #' generate random BKT parameters
-#' 
-#' @return a BKT parameter object with parameters chosen uniformly at random in [0,1] for 
+#'
+#' @return a BKT parameter object with parameters chosen uniformly at random in [0,1] for
 #' the init and learn parameters, and in [0,0.5] for the guess and slip parameters
-#'@export 
+#'@export
 bkt.random.params <- function()
 {
   as.bkt.params(list(init=runif(1), learn=runif(1), guess=runif(1,0,0.5), slip=runif(1,0,0.5)))
 }
 
 #' distance between two BKT parameter vectors
-#' 
+#'
 #' Calculates the Euclidean distance between two sets of BKT parameters
 #' @param p1 a BKT parameter object, or a matrix of same
 #' @param p2 another BKT parameter object, or a matrix of same
@@ -65,7 +65,7 @@ bkt.param.dist <- function(p1, p2)
 }
 
 #' root mean squared error for a BKT model
-#' 
+#'
 #' Calculates the root mean squared error for the BKT model
 #' with the given parameters on the given data.
 #' @param opps the opportunity table of observed student actions
@@ -78,7 +78,7 @@ bkt.rmse <- function(opps, params)
 }
 
 #' sum of squared errors of the BKT model
-#' 
+#'
 #' Calculates a sum of squared errors for the BKT model
 #' with the given parameters on the given data.
 #'
@@ -93,10 +93,10 @@ bkt.sse <- function(opps, params)
 }
 
 #' #' sum of squared errors of the BKT model
-#' 
+#'
 #' Calculates a sum of squared errors for the BKT model
 #' with the given parameters on the given data.
-#' @param seq a vector of observed student actions 
+#' @param seq a vector of observed student actions
 #' as occurs in a single row of an opportunity table
 #' @param params a BKT parameter object
 #' @return the sum of squared errors for the BKT model's predictions on this data
@@ -117,10 +117,10 @@ bkt.sse.seq <- function(seq, params)
 #' @param params a BKT parameter object
 #' @return a matrix with the same dimensions as \code{opps}, giving the running estimates of the
 #' probability of a correct response predicted by the BKT model with parameters \code{params}
-#' for the student represented by the corresponding row immediately prior to 
+#' for the student represented by the corresponding row immediately prior to
 #' corresponding column in \code{opps}. That is, the if the \code{(i,j)} entry in the matrix
 #' had a value of 0.7, this means that the BKT algorithm with the given parameters predicts that
-#' the i-th student has a 70 percent chance of producing a correct response on the j-th entry, given 
+#' the i-th student has a 70 percent chance of producing a correct response on the j-th entry, given
 #' the j-1 previously observed actions.
 #' @export
 bkt.pcorrect <- function(opps, params)
@@ -131,15 +131,15 @@ bkt.pcorrect <- function(opps, params)
 }
 
 #' probability of correct responses predicted by BKT
-#' 
+#'
 #' Calculates the probability of correct responses predicted by BKT
 #' given the observed actions in a sequence of student opportunities
-#' @param opps a vector of observed student actions 
+#' @param opps a vector of observed student actions
 #' as occurs in a single row of an opportunity table
 #' @param params a BKT parameter object
 #' @return a vector with the same length as \code{opps}, giving running estimates of the
 #' probability of a correct response predicted by the BKT model with parameters \code{params}
-#' for the student given the prior actions in \code{opps}. 
+#' for the student given the prior actions in \code{opps}.
 #' @export
 #' @keywords internal
 bkt.pcorrect.seq <- function(opps, params)
@@ -154,35 +154,35 @@ bkt.pcorrect.seq <- function(opps, params)
 
 #' probability of student knowledge predicted by BKT
 #'
-#' Calculates the probability of a student being in the known state, 
+#' Calculates the probability of a student being in the known state,
 #' predicted by BKT given the observed actions in a table of student opportunities
 #' @param opps the opportunity table of observed student actions
 #' @param params a BKT parameter object
-#' @return a matrix with the same rows as \code{opps}, and \code{ncol(opps)+1} columns, 
-#' giving running estimates of the probability of student knowledge predicted by the BKT model 
-#' with parameters \code{params} for the student represented by the corresponding row immediately prior to 
+#' @return a matrix with the same rows as \code{opps}, and \code{ncol(opps)+1} columns,
+#' giving running estimates of the probability of student knowledge predicted by the BKT model
+#' with parameters \code{params} for the student represented by the corresponding row immediately prior to
 #' corresponding column in \code{opps}. That is, the if the \code{(i,j)} entry in the matrix
 #' had a value of 0.7, this means that the BKT algorithm with the given parameters predicts that
-#' the i-th student has a 70 percent chance of being in the  on the j-th entry, given 
+#' the i-th student has a 70 percent chance of being in the  on the j-th entry, given
 #' the j-1 previously observed actions.
 #' @export
 bkt.pknown <- function(opps, params)
 {
   if(is.vector(opps))
-    return(bkt.pknown.seq(opps,params))
+    return(bkt.pknown.seq(opps, params))
   ### t(apply(opps, 1, function(stu.seq) { bkt.pknown.seq(stu.seq, params)}))
 }
 
 #' probability of student knowledge predicted by BKT
 #'
-#' Calculates the probability of a student being in the known state, 
+#' Calculates the probability of a student being in the known state,
 #' predicted by BKT given the observed actions in a sequence of student opportunities
-#' @param opps a vector of observed student actions 
+#' @param opps a vector of observed student actions
 #' as occurs in a single row of an opportunity table
 #' @param params a BKT parameter object
 #' @return a vector with the same length as \code{opps}, giving running estimates of the
 #' probability of student knowledge predicted by the BKT model with parameters \code{params}
-#' for the student given the prior actions in \code{opps}. 
+#' for the student given the prior actions in \code{opps}.
 #' @export
 #' @keywords internal
 bkt.pknown.seq <- function(opps, params)
@@ -199,7 +199,7 @@ bkt.pknown.seq <- function(opps, params)
 }
 
 #' updates a BKT estimate of student knowledge
-#' 
+#'
 #' applies the BKT algorithm to update the estimate of student knowledge given a single observed action
 #' @param result an observed student action
 #' @param params a BKT parameter object
@@ -216,5 +216,5 @@ bkt.posterior.pknown <- function(result, params, prior.pknown=params$init)
   posterior[correct] = (prior.pknown[correct] * (1.0 - params$slip)) / ((prior.pknown[correct] * (1.0 - params$slip)) + ((1.0 - prior.pknown[correct]) * params$guess))
   posterior[!correct] = (prior.pknown[!correct] * params$slip) / ((prior.pknown[!correct] * params$slip) + ((1.0 - prior.pknown[!correct]) * (1.0 - params$guess)))
   # posterior with learning
-  (posterior + (1.0 - posterior)*params$learn) 
+  (posterior + (1.0 - posterior) * params$learn)
 }
