18827677e1a63d246ff899b5a1726ee7dc4b41d8
Tuan Hoang <tuan.hmt@outlook.com>
2018-09-15 15:06:53 +0700

    fix coding style

--- bkt/R/bkt-hmm.R
+++ bkt/R/bkt-hmm.R
@@ -1,23 +1,23 @@
 # convert hmm to bkt params
 bkt.hmm.to.bkt <- function (hmm) {
-  bkt.as.params( c( init=as.numeric( hmm$startProbs[2] ),
-                    learn=as.numeric( hmm$transProbs[1,2] ),
-                    guess=as.numeric( hmm$emissionProbs[1,2] ),
-                    slip=as.numeric( hmm$emissionProbs[2,1] ) ) )
+  bkt.as.params(c(init=as.numeric(hmm$startProbs[2]),
+                    learn=as.numeric(hmm$transProbs[1,2]),
+                    guess=as.numeric(hmm$emissionProbs[1,2]),
+                    slip=as.numeric(hmm$emissionProbs[2,1])))
 }
 
 # convert bkt to hmm params
 # assumption that the knowlegde is unforgetable so mastered to unmasterd is zero
-bkt.to.hmm <- function( params=bkt.random.params() )
+bkt.to.hmm <- function(params=bkt.random.params())
 {
-  if( ! is.vector(params) )
-    params = bkt.as.params( params )
+  if(!is.vector(params))
+    params = bkt.as.params(params)
   bkt.init.hmm(c("unmastered","mastered"),
            c("1","2"),
            c(1.0 - params$init, params$init),
            rbind(c(1.0 - params$learn, params$learn),
                   c(NA, 1.0)),
-           rbind( c(1.0 - params$guess, params$guess),
+           rbind(c(1.0 - params$guess, params$guess),
                   c(params$slip, 1.0 - params$slip)))
 }
 
@@ -39,7 +39,7 @@ bkt.init.hmm <- function(States, Symbols, startProbs=NULL, transProbs=NULL, emis
               emissionProbs=E))
 }
 
-forward = function(hmm, observation) {
+forward <- function(hmm, observation) {
   hmm$transProbs[is.na(hmm$transProbs)]       = 0
   hmm$emissionProbs[is.na(hmm$emissionProbs)] = 0
   nObservations  = length(observation)
@@ -47,16 +47,16 @@ forward = function(hmm, observation) {
   f          = array(NA,c(nStates,nObservations))
   dimnames(f)= list(states=hmm$States,index=1:nObservations)
   # Init
-  for(state in hmm$States) {
+  for (state in hmm$States) {
     f[state,1] = log(hmm$startProbs[state] * hmm$emissionProbs[state,observation[1]])
   }
   # Iteration
-  for(k in 2:nObservations) {
-    for(state in hmm$States) {
+  for (k in 2:nObservations) {
+    for (state in hmm$States) {
       logsum = -Inf
-      for(previousState in hmm$States) {
+      for (previousState in hmm$States) {
         temp   = f[previousState,k-1] + log(hmm$transProbs[previousState,state])
-        logsum = logadd(logsum, temp)
+        logsum = logadd(temp, logsum)
       }
       f[state,k] = log(hmm$emissionProbs[state,observation[k]]) + logsum
     }
@@ -64,7 +64,7 @@ forward = function(hmm, observation) {
   return(f)
 }
 
-backward = function(hmm, observation) {
+backward <- function(hmm, observation) {
   hmm$transProbs[is.na(hmm$transProbs)]       = 0
   hmm$emissionProbs[is.na(hmm$emissionProbs)] = 0
   nObservations  = length(observation)
@@ -72,16 +72,16 @@ backward = function(hmm, observation) {
   b          = array(NA,c(nStates,nObservations))
   dimnames(b)= list(states=hmm$States,index=1:nObservations)
   # Init
-  for(state in hmm$States) {
+  for (state in hmm$States) {
     b[state,nObservations] = log(1)
   }
   # Iteration
-  for(k in (nObservations-1):1) {
-    for(state in hmm$States) {
+  for (k in (nObservations-1):1) {
+    for (state in hmm$States) {
       logsum = -Inf
-      for(nextState in hmm$States) {
+      for (nextState in hmm$States) {
         temp   = b[nextState,k+1] + log(hmm$transProbs[state,nextState]*hmm$emissionProbs[nextState,observation[k+1]])
-        logsum = logadd(logsum, temp)
+        logsum = logadd(temp, logsum)
       }
       b[state,k] = logsum
     }
@@ -89,12 +89,13 @@ backward = function(hmm, observation) {
   return(b)
 }
 
+
 baumWelch = function(hmm, observation, maxIterations=100, delta=1E-9, pseudoCount=0) {
   tempHmm = hmm
   tempHmm$transProbs[is.na(hmm$transProbs)]       = 0
   tempHmm$emissionProbs[is.na(hmm$emissionProbs)] = 0
   diff = c()
-  for(i in 1:maxIterations) {
+  for (i in 1:maxIterations) {
     # Expectation Step (Calculate expected Transitions and Emissions)
     bw = baumWelchRecursion(tempHmm, observation)
     T  = bw$TransitionMatrix
@@ -109,7 +110,7 @@ baumWelch = function(hmm, observation, maxIterations=100, delta=1E-9, pseudoCoun
     diff = c(diff, d)
     tempHmm$transProbs    = T
     tempHmm$emissionProbs = E
-    if(d < delta) {
+    if (d < delta) {
       break
     }
   }
@@ -126,24 +127,20 @@ baumWelchRecursion = function(hmm, observation) {
   f = forward(hmm,  observation)
   b = backward(hmm, observation)
   probObservations = f[1,length(observation)]
-  for(i in 2:length(hmm$States)) {
+  for (i in 2:length(hmm$States)) {
     j = f[i,length(observation)]
-    if(j > - Inf)
-    {
+    if (j > - Inf) {
       probObservations = j + log(1+exp(probObservations-j))
     }
   }
-  for(x in hmm$States) {
-    for(y in hmm$States)
-    {
+  for (x in hmm$States) {
+    for (y in hmm$States) {
       temp = f[x,1] + log(hmm$transProbs[x,y]) +
         log(hmm$emissionProbs[y,observation[1+1]]) + b[y,1+1]
-      for(i in 2:(length(observation)-1))
-      {
+      for (i in 2:(length(observation)-1)) {
         j = f[x,i] + log(hmm$transProbs[x,y]) +
           log(hmm$emissionProbs[y,observation[i+1]]) + b[y,i+1]
-        if(j > - Inf)
-        {
+        if (j > - Inf) {
           temp = j + log(1+exp(temp-j))
         }
       }
@@ -151,17 +148,13 @@ baumWelchRecursion = function(hmm, observation) {
       TransitionMatrix[x,y] = temp
     }
   }
-  for(x in hmm$States) {
-    for(s in hmm$Symbols)
-    {
+  for (x in hmm$States) {
+    for (s in hmm$Symbols) {
       temp = -Inf
-      for(i in 1:length(observation))
-      {
-        if(s == observation[i])
-        {
+      for (i in 1:length(observation)) {
+        if (s == observation[i]) {
           j = f[x,i] + b[x,i]
-          if(j > - Inf)
-          {
+          if (j > - Inf) {
             temp = j + log(1+exp(temp-j))
           }
         }
